61958c9e75d2d9a836a7f34e034259d442fe37e2
Betul Zora <zorabtl@gmail.com>
2023-08-18 18:18:26 -0500

    Heapify Live Application

---
+++ src/test/java/live_class_notes/week8_Heap/HeapifyTestApp.java
@@ -0,0 +1,12 @@
+package live_class_notes.week8_Heap;
+
+
+public class HeapifyTestApp {
+    public static void main(String[] args) {
+        int[] numbers={1,2,5,6,8,21,0,-1,-20,87};
+        MyHeap heap=new MyHeap(numbers);
+        heap.printHeap();
+
+    }
+
+}
